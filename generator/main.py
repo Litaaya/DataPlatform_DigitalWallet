83abba7d72f792507be296fe47a6c8cd955b9a60
Litaaya <minhquans2810@gmail.com>
2026-06-17 20:16:33 +0700

    [FIX] Fix conflicts between documentation rules and data structures

--- generator/main.py
+++ generator/main.py
@@ -31,18 +31,28 @@ def delivery_report(err, msg):
     else:
         logging.info(f"Successfully sent to {msg.topic()} [Partition: {msg.partition()}]")
 
+past_purchases = []
+
 # Mock Data engine
 def generate_transaction():
+    global past_purchases
     txn_types = ['TOPUP', 'PURCHASE', 'REFUND', 'TRANSFER', 'ADJUSTMENT']
     chosen_type = random.choice(txn_types)
+
+    if chosen_type == 'REFUND' and not past_purchases:
+        chosen_type = 'PURCHASE'
+
     event_id = str(uuid.uuid4())
     customer_id = f"CUST_{random.randint(1000, 1999)}"
     account_id = f"ACC_{customer_id.split('_')[1]}"
     amount = round(random.uniform(5.0, 500.0), 2)
     event_time = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
     direction = 'CREDIT' if chosen_type in ['TOPUP', 'REFUND'] else 'DEBIT'
-    ref_txn_id = str(uuid.uuid4()) if chosen_type == 'REFUND' else None
     transfer_id = f"TRF_{random.randint(100000, 999999)}" if chosen_type == 'TRANSFER' else None
+
+    ref_txn_id = None
+    if chosen_type == 'REFUND':
+        ref_txn_id = random.choice(past_purchases)
 
     # Anomaly injection
     anomaly_dice = random.random()
@@ -67,6 +77,14 @@ def generate_transaction():
         "event_time": event_time
     }
 
+    if chosen_type == 'ADJUSTMENT':
+        txn_payload["admin_id"] = f"ADM_00{random.randint(1, 5)}"
+        txn_payload["audit_reason"] = random.choice([
+            "Technical correction due to network gateway timeout",
+            "Manual wallet re-balancing requested by customer support",
+            "Clearing operational ledger discrepancy anomalies"
+        ])
+
     return txn_payload, error_note, chosen_type
 
 # Main streaming pipeline
@@ -76,6 +94,7 @@ try:
         payload, note, txn_type = generate_transaction()
 
         if txn_type == 'TRANSFER' and "ANOMALY" not in note:
+            payload['ref_txn_id'] = None
             payload['direction'] = 'DEBIT'
             producer.produce(TOPIC_NAME, key=payload['event_id'], value=json.dumps(payload), callback=delivery_report)
 
@@ -84,9 +103,12 @@ try:
             receiver_payload['account_id'] = f"ACC_{random.randint(2000, 2999)}"
             receiver_payload['customer_id'] = f"CUST_{receiver_payload['account_id'].split('_')[1]}"
             receiver_payload['direction'] = 'CREDIT'
+            receiver_payload['ref_txn_id'] = None
             producer.produce(TOPIC_NAME, key=receiver_payload['event_id'], value=json.dumps(receiver_payload), callback=delivery_report)
         else:
             producer.produce(TOPIC_NAME, key=payload['event_id'], value=json.dumps(payload), callback=delivery_report)
+            if txn_type == 'PURCHASE' and note == "CLEAN":
+                past_purchases.append(payload['event_id'])
 
         if note != "CLEAN":
             logging.warning(f"Deliberate error has been intentionally created: {note} | ID: {payload['event_id']}")
